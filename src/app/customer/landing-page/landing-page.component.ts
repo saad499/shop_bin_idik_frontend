b8fc9a8fe1a3ebdc4d00f35b5463602ac8b59d75
saad499 <saadkammouss@gmail.com>
2025-12-22 01:01:25 +0100

    make design page accueil client

--- src/app/customer/landing-page/landing-page.component.ts
+++ src/app/customer/landing-page/landing-page.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  sidebarCollapsed = false;
+  carouselIndex = 0;
+  carouselImages = [
+    'assets/images/video-games.jpg',
+    'assets/images/supermarket.jpg',
+    'assets/images/sport-loisir.jpg',
+    'assets/images/warm-clothes.png',
+    'assets/images/clot.jpg',
+    'assets/images/electronics.jpg',
+    'assets/images/sneakers.jpg',
+    'assets/images/jouet.jpg'
+  ];
+
+  toggleSidebar(): void {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
+
+  nextImage(): void {
+    this.carouselIndex = (this.carouselIndex + 1) % this.carouselImages.length;
+  }
+
+  prevImage(): void {
+    this.carouselIndex = this.carouselIndex === 0 
+      ? this.carouselImages.length - 1 
+      : this.carouselIndex - 1;
+  }
+
+  goToImage(index: number): void {
+    this.carouselIndex = index;
+  }
+}
